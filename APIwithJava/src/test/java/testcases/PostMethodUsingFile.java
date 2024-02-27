403eb593e960246feea9d3f66d23aefd07196d76
Dell <Dell@DESKTOP-2U7567B>
2024-02-27 08:44:34 +0530

    1st commit

---
+++ APIwithJava/src/test/java/testcases/PostMethodUsingFile.java
@@ -0,0 +1,33 @@
+package testcases;
+
+import static io.restassured.RestAssured.*;
+
+import java.io.*;
+
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class PostMethodUsingFile {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		File f=new File("../APIwithJava/RequestBody.json");
+		FileReader fr = new FileReader(f);
+		JSONTokener js = new JSONTokener(fr);
+		JSONObject j = new JSONObject(js);
+//		System.out.println(j);
+		
+	Response res=
+	 given()
+	 .contentType(ContentType.JSON)
+	 .body(j.toString())
+	 .when()
+	 .post("http://localhost:4000/EmployeeInfo");
+	
+	System.out.println("Status Code is "+res.statusCode());
+	System.out.println(res.asPrettyString());
+	}
+
+}
